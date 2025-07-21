09bc3d170116594043486c4a8a2d1a27a97c9345
dei-30 <deivisfonseca3006@gmail.com>
2025-07-21 11:57:39 -0400

    Add files via upload

---
+++ avtividad 1.py
@@ -0,0 +1,7 @@
+# tarea
+nombre = input ("¿cual es tu nombre?")
+edad = input ("¿cuantos años tienes?")
+pelicula = input("¿cual es tu pelicula favorita?")
+edad_num = int (edad) 
+print (f"Hola {nombre}, tienes{edad}, en 10 años tendras{edad_num + 10} tu pelicula favorita es{pelicula}") 
+ 
